7aa682999ba35d67a63b28a5e8de233c8d798641
RayHaha <g820324@gmail.com>
2018-10-30 18:12:23 -0700

    handle POST

--- server.js
+++ server.js
@@ -3,14 +3,20 @@ var url = require("url");
 
 function start(route, handle) {
   function onRequest(request, response) {
+  	var postData = "";
   	var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
 
-    route(handle, pathname);
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
+    request.setEncoding("utf8");
+    request.addListener("data", function(postDataChunk){
+    	postData += postDataChunk;
+    	console.log("Received POST data chunk '"+
+      	postDataChunk + "'.");
+    });
+    request.addListener("end", function(){
+    	route(handle, pathname, response, postData);
+    });
+    
   }
 
   http.createServer(onRequest).listen(8888);
@@ -18,3 +24,4 @@ function start(route, handle) {
 }
 
 exports.start = start;
+
